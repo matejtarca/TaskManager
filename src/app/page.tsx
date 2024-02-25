bc07282c135b3aff0072239c8e6441ade41208b1
Matej Tarca <matej.tarca@kiwi.com>
2024-02-25 23:32:38 +0100

    feat(auth): redirect from home to sign in if not signed in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Link from "next/link";
 import TaskList from "@/scenes/TaskList/TaskList";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  return (
-    <div>
-      <TaskList />
-    </div>
-  );
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/sign-in");
+  }
+  return <TaskList />;
 }
