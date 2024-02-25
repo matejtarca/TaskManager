e94eeaa930039f0e9de5722a4101839e54bb7cd9
Matej Tarca <matej.tarca@kiwi.com>
2024-02-25 20:06:19 +0100

    feat(auth): setup next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        console.log(credentials);
+        return null;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
