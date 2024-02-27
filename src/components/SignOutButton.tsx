85a141404f6073c1b7ca1285326fdbd32228edb2
Matej Tarca <matej.tarca@kiwi.com>
2024-02-27 17:16:06 +0100

    fix(eslint): fix eslint errors

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -5,7 +5,11 @@ import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 
 const SignOutButton = () => {
-  return <Button variant="secondary" size="sm" onClick={() => signOut()}>Sign out</Button>;
+  return (
+    <Button variant="secondary" size="sm" onClick={() => signOut()}>
+      Sign out
+    </Button>
+  );
 };
 
 export default SignOutButton;
