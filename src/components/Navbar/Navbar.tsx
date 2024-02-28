af5039c1ccbad25ae233c4b4cd7f645243c234a7
Matej Tarca <matej.tarca@kiwi.com>
2024-02-28 19:49:50 +0100

    feat(app): implement dark mode switch

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/server/auth/authOptions";
-import SignOutButton from "@/components/SignOutButton";
+import SignOutButton from "@/components/Navbar/components/SignOutButton";
 import Link from "next/link";
-import { Heading } from "./ui/heading";
-import { Button } from "./ui/button";
+import { Heading } from "../ui/heading";
+import { Button } from "../ui/button";
+import { ThemeToggle } from "@/components/Navbar/components/ThemeToggle";
 
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
   return (
-    <div className="flex items-center p-5 fixed w-screen bg-white">
+    <div className="flex items-center p-5 fixed w-screen bg-background">
       <div className="flex-1"></div>
       <div className="flex-1 max-w-xs mx-auto text-center">
         <Link href="/">
@@ -18,7 +19,7 @@ const Navbar = async () => {
           </Heading>
         </Link>
       </div>
-      <div className="flex-1 flex justify-end">
+      <div className="flex-1 flex justify-end gap-4">
         {session ? (
           <div className="flex items-center gap-4">
             <span>
@@ -36,6 +37,7 @@ const Navbar = async () => {
             </Link>
           </div>
         )}
+        <ThemeToggle />
       </div>
     </div>
   );
